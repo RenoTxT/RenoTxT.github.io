2ae8d6378e98ebaa3afbd43c57d0939a7d976582
RenoTxT <reno.mardiputra@gmail.com>
2024-08-21 19:26:02 +0700

    base_website

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import navbar from "./components/navbar.vue";
+import headerer from "./components/header.vue";
+import content from "./components/content.vue";
+
+</script>
+
+<template>
+  <main>
+    <navbar />
+  </main>
+  <header>
+    <headerer />
+  </header>
+  <section>
+    <content/>
+  </section>
+</template>
+
+<style scoped>
+*{
+  background-color: black;
+}
+</style>
