1bda99f649f9592fd1b8a44f240833fdb4180067
RenoTxT <reno.mardiputra@gmail.com>
2024-08-22 06:11:57 +0700

    test-03

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import './assets/main.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
