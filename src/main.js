cbe2d7a349a1bc794415c9f24459219ed7c41041
RenoTxT <reno.mardiputra@gmail.com>
2024-08-22 11:56:01 +0700

    test-importmap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r'
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy'
 
 import { createApp } from 'vue'
 import App from './App.vue'
