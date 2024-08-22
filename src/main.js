fffcb6de522c44049b1191ee599b1785d44f62c0
RenoTxT <reno.mardiputra@gmail.com>
2024-08-22 12:10:18 +0700

    back-to-tedault2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-import '/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
