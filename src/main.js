cb86ce3260514f2aa717073f55b430405e6e2d33
RenoTxT <reno.mardiputra@gmail.com>
2024-08-22 12:51:34 +0700

    test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 import { createApp } from 'vue'
 import App from './App.vue'
