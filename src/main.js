f083da0f8df6f38f17fd09db00555bc47b756765
RenoTxT <reno.mardiputra@gmail.com>
2024-08-22 07:02:21 +0700

    final-commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
