2a1567a11c1f1d501749f5d42ba08b6763f1e58b
RenoTxT <reno.mardiputra@gmail.com>
2024-08-22 12:09:05 +0700

    back-to-default

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './assets/main.css'
-import 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js'
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js'
+import '/bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
