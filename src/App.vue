97879c573027fc2160a5d69d58008f53f1fc4502
RenoTxT <reno.mardiputra@gmail.com>
2024-08-22 10:05:28 +0700

    test-vite

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <script setup>
-import navigation from "./components/navbar.vue";
-import heading from "./components/header.vue";
-import content from "./components/content.vue";
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="container-fluid nav-space">
-    <navigation/>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
-  <div class="container-fluid">
-    <!-- <heading/> -->
-    <content/>
-  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-*{
-  background-color: black;
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-
-.nav-space{
-  margin-bottom: 15vh;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
